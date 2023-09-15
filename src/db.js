15fc12e1c353a7ce86f44c2651c298d894014518
LeoCodeDev <alb.mar.cta@gmail.com>
2023-09-15 12:48:13 -0500

    deleting redounding lines

--- src/db.js
+++ src/db.js
@@ -4,17 +4,9 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, BD_DEPLOY } = process.env;
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//     force: false,
-//   }
-// );
-
 const sequelize = new Sequelize(
-  BD_DEPLOY,
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    BD_DEPLOY,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
